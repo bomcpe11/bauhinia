d3471bb3ad51fe4bd249d2bec306b0eaa9faaa42
noobofwar <jnattapop@gmail.com>
2015-07-28 23:16:09 +0700

    next page

--- wp-content/themes/custom1/page-annoucement.php
+++ wp-content/themes/custom1/page-annoucement.php
@@ -1,18 +1,22 @@
 <?php get_header(); ?>
 <div class="annoucement">
 	    	
-			
-			<?php query_posts("cat=7&posts_per_page=1&post_status=publish,future");?>
+			<?php
+			$page = (get_query_var('paged')) ? get_query_var('paged') : 1;
+			?>
+			<?php query_posts("cat=7&posts_per_page=1&paged='.$page");?>
 			
 			<?php if (have_posts()) : ?>
 			<table>
-			<?php while (have_posts()) : the_post(); ?>
+			<?php  while (have_posts()) : the_post(); ?>
 			
 				<tr>
-					<h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
+					<h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
 				</tr>
 			<?php endwhile; ?>
 			</table>
+		<p><?php next_posts_link(); ?></p>
+	<p><?php previous_posts_link(); ?></p>
 			<?php else : ?>
 			
 			  <h3 class="topTitle"><a href="<?php the_permalink() ?>">Blog data nothing</a></h3>
