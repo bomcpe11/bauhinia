174bd88fe2ebed082d2c02c1c324df6fff2b4839
noobofwar <jnattapop@gmail.com>
2015-08-06 16:56:11 +0700

    update wordpress,plugin install plugin "meta slider" edit homepage for 3 group and 1 slide

--- wp-content/themes/custom1/page-home.php
+++ wp-content/themes/custom1/page-home.php
@@ -1,11 +1,10 @@
 <?php get_header(); ?>
-<div class="annoucement">
-	    	
-			<?php
-			$pagenum = get_query_var('paged',1);
-			
-			?>
-			<?php query_posts("cat=7&posts_per_page=5&paged=$pagenum");?>
+<div class="annoucementhome">
+	    	<?php 
+    echo do_shortcode("[metaslider id=147]"); 
+?>
+
+			<?php query_posts("cat=7&posts_per_page=5");?>
 			
 			<?php if (have_posts()) : ?>
 			
@@ -23,8 +22,65 @@
 					
 				</div>
 			<?php endwhile; ?>
-		<p><?php next_posts_link(); ?></p>
-	<p><?php previous_posts_link(); ?></p>
+			<?php else : ?>
+			
+			  <h3 class="topTitle"><a href="<?php the_permalink() ?>">Blog data nothing</a></h3>
+			  <p></p>
+			<!-- Closes topPost -->
+			
+			<?php endif; ?>
+			
+			<?php wp_reset_query(); ?>
+</div>
+<div class="coursehome">			
+			<?php query_posts("cat=8&posts_per_page=5");?>
+			
+			<?php if (have_posts()) : ?>
+			
+			<?php  while (have_posts()) : the_post(); ?>
+			
+				<div>
+					<h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
+					
+					<?php 
+					$content = get_the_content();
+					$content = strip_tags($content);
+					echo substr($content, 0, 100);
+					?>
+					<a href='<?php the_permalink() ?>'>...></a>
+					
+				</div>
+			<?php endwhile; ?>
+			<?php else : ?>
+			
+			  <h3 class="topTitle"><a href="<?php the_permalink() ?>">Blog data nothing</a></h3>
+			  <p></p>
+			<!-- Closes topPost -->
+			
+			<?php endif; ?>
+			
+			<?php wp_reset_query(); ?>
+</div>
+<div class="abouthome">			
+			<?php query_posts("cat=9&posts_per_page=5");?>
+			
+			<?php if (have_posts()) : ?>
+			
+			<?php  while (have_posts()) : the_post(); ?>
+			
+				<div>
+					<h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
+					
+					<?php 
+					$content = get_the_content();
+					$content = strip_tags($content);
+					echo substr($content, 0, 100);
+					?>
+					<a href='<?php the_permalink() ?>'>...></a>
+					
+				</div>
+			<?php endwhile; ?>
+
 			<?php else : ?>
 			
 			  <h3 class="topTitle"><a href="<?php the_permalink() ?>">Blog data nothing</a></h3>
